01bee159dbb8df8cc3645ea9d9023ffff449a07d
Ram Vaid <ramsandeepvaid@gmail.com>
2025-06-04 15:39:30 +0530

    data Validation

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -15,6 +15,11 @@ FILE_NAME:str="phishingData.csv"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
 
+##To validate number of columns we need to compare the train and test files uske liye humne ye schema.yaml file bnai hai
+##isme hum bta denge kitne cols chahiye aur isse compare kra lega model hmara train aur test files ko
+
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
 
 '''
 Data Ingestion related constants start with DATA_INGESTION VAR NAME
@@ -26,3 +31,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingesion"
 DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
 DATA_INGESTION_INGESTED_DIR:str="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2
+
+
+'''
+Data Validation related constants start with DATA_VALIDATION VAR NAME
+'''
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
