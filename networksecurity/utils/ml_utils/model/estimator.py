66fb93f224e908956b012044053fc6b1930ddf7d
Ram Vaid <ramsandeepvaid@gmail.com>
2025-06-05 18:33:08 +0530

    model training

---
+++ networksecurity/utils/ml_utils/model/estimator.py
@@ -0,0 +1,27 @@
+from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR,MODEL_FILE_NAME
+
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+class NetworkModel:
+    def __init__(self,preprocessor,model):
+        '''
+        this will initialise the model and preprocessor'''
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def predict(self,x):
+        '''
+        this will be used to predict values'''
+        try:
+            x_transform=self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
