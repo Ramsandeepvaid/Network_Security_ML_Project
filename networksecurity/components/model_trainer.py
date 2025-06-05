6a315332c2a7d010e1d2ef608aebf03cc7563d61
Ram Vaid <ramsandeepvaid@gmail.com>
2025-06-05 19:02:26 +0530

    mlflows

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import mlflow
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -31,6 +32,18 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+
         
     def train_model(self,X_train,y_train,x_test,y_test):
         '''
@@ -83,11 +96,20 @@ class ModelTrainer:
         y_train_pred=best_model.predict(X_train)
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+
+        ##Track the experiments with mlFlow
+        '''
+        MLflow helps you organize, track, and deploy your machine learning experiments and models efficiently.
+        '''
+        self.track_mlflow(best_model,classification_train_metric)
+
+
         
 
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
